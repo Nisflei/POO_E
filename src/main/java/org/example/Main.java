5462a6d62a67be5fbc76d348ef976a9293452ea6
Nisflei <ncgaloni@hotmail.com>
2023-05-15 13:42:35 -0300

    novo

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,6 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Germinare TECH 2o. E ");
+        System.out.println("Germinare TECH 2o. Ano ... Sempre superando");
     }
 }
